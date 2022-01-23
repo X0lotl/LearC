556d26a87e4d1c73f385c9ace57ce40471786e00
X0lotl <danil200319771@gmail.com>
2022-01-24 00:23:21 +0200

    try to do revert calculator but i fucked up

--- NumeralSystemCalculator/NumeralSystemCalculator/Source.cpp
+++ NumeralSystemCalculator/NumeralSystemCalculator/Source.cpp
@@ -1,13 +1,32 @@
 #include <iostream>
 #include <string>
+#include <cmath>
 
-void calculateLess10(int inputnumber, int numeralSystem) {
+char characters[16] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+int findElement(char element) {
+	for (int i = 0; i < sizeof(characters); i++) {
+		if (characters[i] == element) {
+			return i;
+		}
+	}
+}
+
+void revertCalculate(std::string inputnumber, int numeralSystem) {
+	int sum = 0;
+	for (int i = 0; inputnumber.size() - i > 0; i++) {		
+		int index = (findElement(inputnumber.c_str()[inputnumber.size() - (i - 1)]));
+		sum += index * pow(numeralSystem, i);
+	}
+	std::cout << "Result is: " << sum << std::endl;
+}
+
+void calculate(int inputnumber, int numeralSystem) {
 	std::string newCharacter = "";
 	std::string oldCharacter = "";
 	std::string temp;
-	for (inputnumber; (inputnumber + numeralSystem) > numeralSystem; inputnumber = inputnumber / numeralSystem) {
+	for (; (inputnumber + numeralSystem) > numeralSystem; inputnumber /= numeralSystem) {
 		temp = oldCharacter;
-		newCharacter = std::to_string(inputnumber % numeralSystem);
+		newCharacter = characters[inputnumber % numeralSystem];
 		oldCharacter = newCharacter + temp;
 		std::cout << inputnumber << " div " << numeralSystem << " = " << inputnumber / numeralSystem << std::endl;
 		std::cout << inputnumber << " mod " << numeralSystem << " = " << newCharacter << std::endl;
@@ -19,13 +38,14 @@ void calculateLess10(int inputnumber, int numeralSystem) {
 
 int main() {
 	int numeralSystem;
-	int inputNumber;
+	std::string inputNumber;
 	std::cout << "Enter input number: ";
 	std::cin >> inputNumber;
-	std::cout << "Enter numberl system: ";
+	std::cout << "Enter numberal system: ";
 	std::cin >> numeralSystem;
-	calculateLess10(inputNumber, numeralSystem);
+	//calculate(inputNumber, numeralSystem);
 
+	revertCalculate(inputNumber, numeralSystem);
 	//std::cout << inputNumber << numeralSystem;
 	return 0;
 }
