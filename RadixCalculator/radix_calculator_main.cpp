15a2f4983e77dfa547bdd9fee4917996dd781477
X0lotl <danil200319771@gmail.com>
2022-01-26 12:03:05 +0200

    refactor code

--- RadixCalculator/radix_calculator_main.cpp
+++ RadixCalculator/radix_calculator_main.cpp
@@ -5,12 +5,14 @@
 
 char characters[16] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
 
-int findElement(char element) {
+int findElement(char element)
+{
     for (int i = 0; i < sizeof(characters); i++) {
         if (characters[i] == element) {
             return i;
         }
     }
+
     return -1;
 }
 
@@ -21,42 +23,51 @@ int search_dot(std::string inputNumber)
             return i;
         }
     }
+
     return -1;
 }
 
-bool has_minus_at_start(std::string inputNumber) {
+bool has_minus_at_start(std::string inputNumber)
+{
     if (inputNumber.c_str()[0] == '-') {
         return true;
     }
+
     return false;
 }
 
-std::string build_rightside(std::string inputNumber, int indexOfDot, int numeralSystem) {
+std::string build_rightside(std::string inputNumber, int indexOfDot, int numeralSystem)
+{
     std::string output = "";
-    int power = -1;
+
     for (int i = 1; i < (inputNumber.size() - indexOfDot); i++) {
         int index = (findElement(inputNumber.c_str()[i + indexOfDot]));
         output = output + std::to_string(index) + " * " + std::to_string(numeralSystem) + " ^ " + std::to_string(-i) +
                  " + ";
-        power--;
     }
+
     return output;
 }
 
-std::string build_left_side(std::string inputNumber, int indexOfDot, int numeralSystem) {
+std::string build_left_side(std::string inputNumber, int indexOfDot, int numeralSystem)
+{
     std::string output = "";
+
     for (int i = 0; i < indexOfDot; i++) {
         int index = (findElement(inputNumber.c_str()[(indexOfDot - (i)) - 1]));
 
         output = std::to_string(index) + " * " + std::to_string(numeralSystem) + " ^ " + std::to_string(i) + " + " +
                  output;
     }
+
     return output;
 }
 
-int revert_calculate_from_n_to_10(std::string inputnumber, int numeralSystem) {
+int revert_calculate_from_n_to_10(std::string inputnumber, int numeralSystem)
+{
     int sum = 0;
     std::string finalString;
+
     for (int i = 0; inputnumber.size() - i > 0; i++) {
         int index = (findElement(inputnumber.c_str()[inputnumber.size() - (i + 1)]));
         sum += index * pow(numeralSystem, i);
@@ -64,53 +75,66 @@ int revert_calculate_from_n_to_10(std::string inputnumber, int numeralSystem) {
                 std::to_string(index) + " * " + std::to_string(numeralSystem) + " ^ " + std::to_string(i) + " + " +
                 finalString;
     }
+
     std::cout << finalString << " = " << sum << std::endl;
     std::cout << "Result is: " << sum << std::endl;
+
     return sum;
 }
 
-std::string build_result(std::string inputNumber, int indexOfDot, int numeralSystem, bool minus) {
+std::string build_result(std::string inputNumber, int indexOfDot, int numeralSystem, bool minus)
+{
     std::string leftSide = build_left_side(inputNumber, indexOfDot, numeralSystem);
     std::string rightSide = build_rightside(inputNumber, indexOfDot, numeralSystem);
     std::string output;
+
     if (indexOfDot != -1) {
         output = leftSide + rightSide;
     } else {
         output = revert_calculate_from_n_to_10(inputNumber, numeralSystem);
     }
+
     if (minus) {
         output = "-(" + output + ")";
     }
+
     return output;
 }
 
-void sum_of_the_base_level(std::string inputNumber, int numeralSystem) {
+void sum_of_the_base_level(std::string inputNumber, int numeralSystem)
+{
     bool minus = has_minus_at_start(inputNumber);
+
     if (minus) {
         inputNumber.erase(0, 1);
     }
+
     int indexOfDot = search_dot(inputNumber);
     std::string output = build_result(inputNumber, indexOfDot, numeralSystem, minus);
     std::cout << output << std::endl;
 }
 
-int calculate_from_10_to_n(int inputnumber, int numeralSystem) {
+int calculate_from_10_to_n(int inputNumber, int numeralSystem)
+{
     std::string newCharacter = "";
     std::string oldCharacter = "";
     std::string temp;
-    for (; (inputnumber + numeralSystem) > numeralSystem; inputnumber /= numeralSystem) {
+
+    for (; (inputNumber + numeralSystem) > numeralSystem; inputNumber /= numeralSystem) {
         temp = oldCharacter;
-        newCharacter = characters[inputnumber % numeralSystem];
+        newCharacter = characters[inputNumber % numeralSystem];
         oldCharacter = newCharacter + temp;
-        std::cout << inputnumber << " div " << numeralSystem << " = " << inputnumber / numeralSystem << std::endl;
-        std::cout << inputnumber << " mod " << numeralSystem << " = " << newCharacter << std::endl;
+        std::cout << inputNumber << " div " << numeralSystem << " = " << inputNumber / numeralSystem << std::endl;
+        std::cout << inputNumber << " mod " << numeralSystem << " = " << newCharacter << std::endl;
         std::cout << "\n";
     }
     std::cout << "Result is: " << oldCharacter;
+
     return atoi(oldCharacter.c_str());
 }
 
-int main() {
+int main()
+{
     int numeralSystem;
     int functionPick;
     std::string inputNumber;
@@ -142,5 +166,6 @@ int main() {
             std::cout << "Fucking idiot please enter 0 or 1 !!!!!!";
         }
     }
+
     return 0;
 }
