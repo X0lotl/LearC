3acf9f3360afcf784112a1191852a370e2572abd
X0lotl <92333793+X0lotl@users.noreply.github.com>
2022-01-28 20:27:56 +0200

    need to go on pc

---
+++ RadixCalculator/cmake-build-debug/CMakeFiles/RadixCalculator.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RadixCalculator.dir/radix_calculator_main.cpp.obj"
+  "CMakeFiles/RadixCalculator.dir/radix_calculator_main.cpp.obj.d"
+  "RadixCalculator.exe"
+  "RadixCalculator.exe.manifest"
+  "RadixCalculator.pdb"
+  "libRadixCalculator.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RadixCalculator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ RadixCalculator/cmake-build-debug/CMakeFiles/RadixCalculator.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for RadixCalculator.
+# This may be replaced when dependencies are built.
